d2bab510e8868851dcdb9c93cb7e7084a2bd6a5e
Joe Masilotti <joe@masilotti.com>
2022-12-13 15:31:20 -0800

    WIP

---
+++ components/email_capture.rb
@@ -0,0 +1,13 @@
+class EmailCapture < ViewComponent::Base
+  include DataHelper
+
+  attr_reader :cta
+
+  def initialize(newsletter = nil, cta: "Subscribe")
+    @newsletter, @cta = newsletter, cta
+  end
+
+  def newsletter
+    @newsletter ||= data(:newsletters).hotwire
+  end
+end
